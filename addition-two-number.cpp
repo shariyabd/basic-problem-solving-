5c3e7e704184dee09fc2b8e6d6483886d5d6d1c0
shariya shuvo <shariya873@gmail.com>
2023-01-21 19:18:03 +0000

    addition of two numbers

---
+++ addition-two-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int number1,number2,sum;
+    cout<<"Enter your first digit"<<endl;
+    cin>>number1;
+    cout<<"Enter your second digit"<<endl;
+    cin>>number2;
+    sum = number1+number2;
+    cout<<"Addition of two number is "<<sum;
+}
