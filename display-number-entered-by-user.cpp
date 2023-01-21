771424f683b4922935aa27a46d28e07adfe49e82
shariya shuvo <shariya873@gmail.com>
2023-01-21 19:08:41 +0000

    display number entered by user

---
+++ display-number-entered-by-user.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int number;
+    cout<<"Enter an intiger number"<<endl;
+    cin>>number;
+
+    cout<<"The number entered is "<<number<<endl;
+}
