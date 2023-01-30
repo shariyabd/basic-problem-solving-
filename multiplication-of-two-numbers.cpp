b50ebbc3969f27ad2e8b79a25e3d3fb79c9dcf35
shariya shuvo <shariya873@gmail.com>
2023-01-30 10:59:06 +0000

    multiplication of two numbers

---
+++ multiplication-of-two-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+   
+   int first,second,multiplication;
+
+   cout<<"Enter your first number :"<<endl;
+   cin>>first;
+   cout<<"Enter your second number :"<<endl;
+   cin>>second;
+   
+   multiplication = first*second;
+   
+   cout<<"Multiplication is "<<multiplication;
+   
+   
+
+    return 0;
+}
