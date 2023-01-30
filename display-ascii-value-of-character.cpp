2ab88a1b270a503caa7084d7dc4b5cccbfe30dab
shariya shuvo <shariya873@gmail.com>
2023-01-30 11:04:10 +0000

    display ascii value of character

---
+++ display-ascii-value-of-character.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char a;
+    
+    cout<<"Enter any character : "<<endl;
+    cin>>a;
+    
+    cout<<"The ascii value of character "<<a<<" is "<<int(a);
+    
+   
+   
+
+    return 0;
+}
