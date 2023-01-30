ee72cfcd413b4a3852e7de3431a374263cb855f0
shariya shuvo <shariya873@gmail.com>
2023-01-30 10:23:38 +0000

    find sizeof int, char, float, double

---
+++ find-sizeof-int-char-float-double.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   
+   cout<<"Size of char "<<sizeof(char)<<"byte"<<endl;
+   cout<<"Size of Int "<<sizeof(int)<<"bytes"<<endl;
+   cout<<"Size of Float "<<sizeof(float)<<"bytes"<<endl;
+   cout<<"Size of Double "<<sizeof(double)<<"bytes"<<endl;
+
+    return 0;
+}
