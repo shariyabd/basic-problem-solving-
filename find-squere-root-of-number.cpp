2a727864f00926d51a0873e2130d3a2c958a98d7
shariya shuvo <shariya873@gmail.com>
2023-01-31 16:24:15 +0000

    squere root of any positive number

---
+++ find-squere-root-of-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    int n,sqr;
+
+    cout<<"Enter a positive value :"<<endl;
+    cin>>n;
+
+    sqr = sqrt(n);
+
+    cout<<"Squere of the number is : "<<sqr;
+}
+
+
