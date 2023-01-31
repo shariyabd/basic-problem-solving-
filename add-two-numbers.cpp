2476f3b7621f91ef672b9dea2e3b052edc88325a
shariya shuvo <shariya873@gmail.com>
2023-01-31 16:16:01 +0000

    summation of two numbers

---
+++ add-two-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1,num2,sum;
+
+    cout<<"Enter your first number :"<<endl;
+    cin>>num1;
+
+    cout<<"Enter your second number :"<<endl;
+    cin>>num2;
+
+    sum = num1+num2;
+    
+    cout<<"The summation of two numbers is : "<<sum;
+}
