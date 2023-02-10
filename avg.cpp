0620e120c8afa2bba6ebbca697c8add39d30a884
shariya shuvo <shariya873@gmail.com>
2023-02-11 05:13:52 +0600

    convert days into year, weeks, days

---
+++ avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,avg,sum;
+    cout<<"Enter your first number :"<<endl;
+    cin>>a;
+    cout<<"Enter your second number :"<<endl;
+    cin>>b;
+    avg = (a+b)/2;
+    cout<<"Avarage of two numbers is :"<<avg<<endl;
+}
